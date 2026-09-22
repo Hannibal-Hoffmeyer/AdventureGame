6e27f504eb31c3e96518385809f3c8efe2b51853
Hannibal-Hoffmeyer <hannibal@hoffmeyer-lippert.com>
2026-09-22 16:21:51 +0200

    room descryption added

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,15 @@
 public class Room {
+    private String description;
+
+    public Room(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
+
+
+
