d7779f560d0a2a121c43364c86e6b4fbfc07f46f
Anas <anas_azghidi@hotmail.com>
2026-09-23 12:59:52 +0200

    "adventure" fully translated to english

--- src/Adventure.java
+++ src/Adventure.java
@@ -3,15 +3,15 @@ public class Adventure {
     private Room currentRoom;
 
     public void AdventureStart() {
-        Room room1 = new Room("Du vågner op i en mørk grotte.");
-        Room room2 = new Room("Du bevæger dig dybere ned i grotten, der ligger en bjørn og sover...");
-        Room room3 = new Room("Du bevæger dig ud af grotten, du ankommer til en lysning i en skov.");
-        Room room4 = new Room("Du går videre og foran dig ser du grotten, hvor du vågnede. Gik du bare i en cirkel?");
-        Room room5 = new Room("Du løser gåden og kommer ind. Indenfor er der kun en kiste; du åbner den. Inde i kisten ligger et kort, der viser vejen ud af skoven. Du finder endelig udgangen af her – tillykke!");
-        Room room6 = new Room("Mens du bevæger dig videre, finder du en bro i nærheden.");
-        Room room7 = new Room("Der ligger en gammel hytte i nærheden, men det ser ikke ud til, at der er nogen; den har været forladt i lang tid.");
-        Room room8 = new Room("Foran tårn står en mand, løs hans gåde for at fortsætte.");
-        Room room9 = new Room("Du forsætter over broen og ser et enormt tårn mød øst.");
+        Room room1 = new Room("You are inside a dark cave. There are two ways out: to the east and to the south.");
+        Room room2 = new Room("You move deeper into the cave and find a bear lying there asleep...");
+        Room room3 = new Room("You find yourself outside the cave; you arrive at a clearing in a forest.");
+        Room room4 = new Room("You find yourself outside of the grotto. Nearby of it there's a deer drinking from a river and a steep mountain.");
+        Room room5 = new Room("Inside the castle there is only a chest and an old skeleton sitting in a chair; you decide to open the chest. Inside of it is a map showing the way out of this forest. You can finally find your way out of here — congratulations!");
+        Room room6 = new Room("As you move on, you find a bridge nearby, it seems safe to walk.");
+        Room room7 = new Room("You find an old hut nearby, but it doesn't look like anyone is there; it has been abandoned for a long time.");
+        Room room8 = new Room("At the tower you see there's no one guarding it and the door is half-open. Maybe someone's inside?");
+        Room room9 = new Room("You continue across the bridge and see a huge tower to the east.");
 
         currentRoom = room1;
         room1.setRoomEast(room2);
@@ -32,14 +32,14 @@ public class Adventure {
         room9.setRoomWest(room8);
         room9.setRoomNorth(room6);
         boolean AdventureStart = true;
-        IO.println("velkommen til Adventure spillet");
+        IO.println("Welcome to the adventure game!");
         IO.println();
-        IO.println("Dine bevægelses knapper er N,S,E,W som svarer til et kompass, derudover kan du skrive LOOK for at observere dine omgivelser");
+        IO.println("Your inputs are N, S, E, and W, which corresponds to a compass. Additionally, you can type LOOK to observe your surroundings.");
         IO.println();
 
         while (AdventureStart) {
 
-            String kommando = IO.readln("Indtast din første kommando!: ").toUpperCase();
+            String kommando = IO.readln("Please input your first commando: ").toUpperCase();
             switch (kommando) {
                 case "LOOK" -> {
                     IO.println(currentRoom.getDescription());
@@ -49,7 +49,7 @@ public class Adventure {
                         currentRoom = currentRoom.getRoomNorth();
                         IO.println(currentRoom.getDescription());
                     } else {
-                        IO.println("Du kan ikke gå nordpå.");
+                        IO.println("You cannot go north.");
                     }
                 }
                 case "S" -> {
@@ -57,7 +57,7 @@ public class Adventure {
                         currentRoom = currentRoom.getRoomSouth();
                         IO.println(currentRoom.getDescription());
                     } else {
-                        IO.println("Du kan ikke gå syd");
+                        IO.println("You cannot go south.");
                     }
                 }
                 case "E" -> {
@@ -65,7 +65,7 @@ public class Adventure {
                         currentRoom = currentRoom.getRoomEast();
                         IO.println(currentRoom.getDescription());
                     } else {
-                        IO.println("Du kan ikke gå øst");
+                        IO.println("You cannot go east.");
                     }
 
                 }
@@ -74,7 +74,7 @@ public class Adventure {
                         currentRoom = currentRoom.getRoomWest();
                         IO.println(currentRoom.getDescription());
                     } else {
-                        IO.println("Du kan ikke gå vest");
+                        IO.println("You cannot go west.");
                     }
                 }
 
