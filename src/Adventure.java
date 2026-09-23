b6310ff609c251143f60cc76c9f8ec4d7eb3b7ca
Anas <anas_azghidi@hotmail.com>
2026-09-23 10:28:49 +0200

    rooms descriptions order corrected

--- src/Adventure.java
+++ src/Adventure.java
@@ -3,15 +3,16 @@ public class Adventure {
     private Room currentRoom;
 
     public void AdventureStart() {
-        Room room1 = new Room("Du vågner op i en mørk grotte");
+        Room room1 = new Room("Du vågner op i en mørk grotte.");
         Room room2 = new Room("Du bevæger dig dybere ned i grotten, der ligger en bjørn og sover...");
-        Room room3 = new Room("du bevæger dig ud af grotten, du ankommer til en lysning i en skov");
+        Room room3 = new Room("Du bevæger dig ud af grotten, du ankommer til en lysning i en skov.");
         Room room4 = new Room("Du går videre og foran dig ser du grotten, hvor du vågnede. Gik du bare i en cirkel?");
-        Room room5 = new Room("Du finder endelig udgangen af denne enorme skov – tillykke!");
+        Room room5 = new Room("Du løser gåden og kommer ind. Indenfor er der kun en kiste; du åbner den. Inde i kisten ligger et kort, der viser vejen ud af skoven. Du finder endelig udgangen af her – tillykke!");
         Room room6 = new Room ("Mens du bevæger dig videre, finder du en bro i nærheden.");
-        Room room7= new Room ("foran broen står en mand, løs hans gåde for at fortsætte");
-        Room room8 = new Room ("Du forsætter over broen og ser et enormt tårn");
-        Room room9 = new Room("Du træder ind i tårnet og ser en guldbelagt kiste, løs gåden for at åbne den");
+        Room room7= new Room ("Der ligger en gammel hytte i nærheden, men det ser ikke ud til, at der er nogen; den har været forladt i lang tid.");
+        Room room8 = new Room ("Foran tårn står en mand, løs hans gåde for at fortsætte.");
+        Room room9 = new Room("Du forsætter over broen og ser et enormt tårn mød øst.");
+
         currentRoom = room1;
 
         boolean AdventureStart = true;
